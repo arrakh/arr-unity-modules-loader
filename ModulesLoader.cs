081343f81a4070997bd202ef17e9715e3e4fd7ff
Arya Rakha <33478892+arrakh@users.noreply.github.com>
2024-07-06 17:06:10 +0700

    init commit

---
+++ ModulesLoader.cs
@@ -0,0 +1,28 @@
+﻿using Arr.EventsSystem;
+using Arr.Utils;
+using UnityEngine;
+using EventHandler = Arr.EventsSystem.EventHandler;
+
+namespace Arr.ModulesSystem
+{
+    public abstract class ModulesLoader : MonoBehaviour
+    {
+        protected abstract BaseModule[] Modules { get; }
+        
+        protected virtual EventHandler EventHandler => GlobalEvents.Instance;
+
+        private ModulesHandler modulesHandler;
+        
+        private void Start()
+        {
+            modulesHandler = new ModulesHandler(Modules, EventHandler);
+            
+            modulesHandler.Start().CatchExceptions();
+        }
+
+        private void OnDestroy()
+        {
+            modulesHandler.Stop().CatchExceptions();
+        }
+    }
+}
